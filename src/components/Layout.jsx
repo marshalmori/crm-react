e1b589cf2f4e8f1976c88a0beb00912b0558a8cd
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-23 19:12:49 -0300

    Deixar o link da página atual destacado em outra cor

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -9,13 +11,19 @@ const Layout = () => {
         </h2>
         <nav className="mt-10">
           <Link
-            className="text-2xl block mt-2 hover:text-blue-400 text-white"
+            className={`${
+              location.pathname === "/" ? "text-yellow-500" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-400`}
             to="/"
           >
             Clientes
           </Link>
           <Link
-            className="text-2xl block mt-2 hover:text-blue-400 text-white"
+            className={`${
+              location.pathname === "/clientes/nuevo"
+                ? "text-yellow-500"
+                : "text-white"
+            } text-2xl block mt-2 hover:text-blue-400`}
             to="/clientes/nuevo"
           >
             Novo Clientes
