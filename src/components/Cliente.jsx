8a4d812b10fd7f4aef6a0d9b53cdb3872fd9382b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-23 21:59:08 -0300

    Iterar sobre o array clientes e apresentar os dados em uma tabela

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,11 @@
+const Cliente = ({ cliente }) => {
+  const { nombre, empresa, email, telefono, id } = cliente;
+
+  return (
+    <tr>
+      <td className="p-5">{nombre}</td>
+    </tr>
+  );
+};
+
+export default Cliente;
